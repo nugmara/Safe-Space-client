8e234e03d138d8a4da1c8ce1ba1fea00f441de68
Dagmara Grabowska <dagmarag0@gmail.com>
2023-03-10 14:35:23 +0100

    trying to figure out how to put the images in the select form

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function Signup() {
+  const [image, setImage] = useState("");
+
+  const avatars = [
+    "https://res.cloudinary.com/dhtrxjdas/image/upload/v1678357943/safe-space-app/Ice-Age-Avatars-1_yeuzft.webp",
+    "https://res.cloudinary.com/dhtrxjdas/image/upload/v1678357943/safe-space-app/Ice-Age-Avatars-2_jamxw0.webp",
+    "https://res.cloudinary.com/dhtrxjdas/image/upload/v1678357943/safe-space-app/Ice-Age-Avatars-3_zzrpeh.webp",
+    "https://res.cloudinary.com/dhtrxjdas/image/upload/v1678357944/safe-space-app/Ice-Age-Avatars-4_nl5y36.webp",
+    "https://res.cloudinary.com/dhtrxjdas/image/upload/v1678357943/safe-space-app/Ice-Age-Avatars-5_vclyp2.webp",
+  ];
+
+  const handleImage = (e) => {
+    setImage(e.target.value);
+  };
   return (
     <div>
       <h1>Sign up</h1>
@@ -44,9 +59,18 @@ function Signup() {
           onChange={undefined}
         />
 
-        <label>Select an image</label>
-        <select name="image">
-          <option value=""></option>
+        <select name="image" value={image} onChange={handleImage} style={{backgroundImage: `url(${image})`}}>
+          <option value="">Select an image</option>
+          {avatars.map((eachAvatar) => (
+            <option key={eachAvatar} value={eachAvatar} style={{backgroundImage: `url(${eachAvatar})`}}>
+              {/* <img
+                src={eachAvatar}
+                alt="ice-age-avatar"
+                width="100px"
+              />  */}
+             {eachAvatar}
+            </option>
+          ))}
         </select>
       </form>
     </div>
